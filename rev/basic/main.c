a6c64df5eebc2da64f3bacd97722aa8448705862
alvinmr <maulanaalvin1@gmail.com>
2021-06-18 02:09:16 +0800

    ganti binari rev

--- rev/basic/main.c
+++ rev/basic/main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
+void init()
+{
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stderr, NULL, _IONBF, 0);
+}
+
 int main()
 {
     int apaini = 100;
     unsigned int yes = 10003434;
     char pass[100];
-
+    init();
     printf("password : ");
     scanf("%s", pass);
     if (strcmp(pass, "hmmFlagNyaAph") == 0)
